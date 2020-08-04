5cf89fe3b9d5ca174bf9908b90b70a1b5511711c
juan251189 <juan2511@hotmail.com>
2020-08-04 23:45:22 +1000

    first commit
    #day 7.
    Player new Audio() succesfull

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <header>
+    <h1>My Music</h1>
+  </header>
+  <main>
+    <section class="player">
+      <h2 class="song-title">{{current.title}} - <span>{{current.artist}}</span></h2>
+
+    </section>
+  </main>
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+    current:{},
+    index:0,
+    songs:[
+      {
+        title:'vacaciones',
+        artist:'Dj David crowd',
+        src:require('./assets/Vacaciones.mp3')
+      },
+      {
+        title:'mix2',
+        artist:'Dj David crowd',
+      src:require('./assets/bonita.mp3')
+      }
+    ],
+    player: new Audio()
+    }
+  },
+  created(){
+    this.current=this.songs[this.index];
+    this.player.src=this.current.src;
+    //  this.player.play();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #EEE;
+
 }
 </style>
