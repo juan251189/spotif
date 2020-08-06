30fd205f9c2a6600c0bd42a2f95cabc377843dcd
juan251189 <juan251189@users.noreply.github.com>
2020-08-07 00:00:28 +1000

    #day9
    tutorial completed

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <main>
     <section class="player">
       <h2 class="song-title">{{current.title}} - <span>{{current.artist}}</span></h2>
-<div class="control">
+<div class="controls">
   <button class="prev" @click="prev">Prev</button>
   <button class="play" v-if="!isPlaying" @click="play">Play</button>
   <button class="pause" v-else @click="pause">Pause</button>
@@ -19,7 +19,7 @@
 
     :key="song.src"
     @click="play(song)" :class="(song.src === current.src) ?  'song playing' : 'song'"
-    >{{song.title}} - {{song.artist}}
+    >{{song.title}} - {{song.artist}} {{isPlaying ? ' -- now playing ' : ""}}
   </button>
   </section>
     </section>
@@ -116,6 +116,100 @@ main{
   width: 100%;
   max-width: 768px;
   margin: 0 auto;
+  padding: 25px;
 
+
+}
+.song-title{
+  color:#53565A;
+  font-size: 30px;
+  font-weight: 700px;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 20px;
+}
+.song-title span{
+  font-weight: 400;
+  font-style: italic;
+}
+
+
+.controls{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding:30ox 15px;
+  margin-bottom: 50px;
+
+}
+
+button{
+  appearance:none;
+  background:none;
+  border:none;
+  outline: none;
+  cursor:pointer;
+}
+
+.play{
+  font-size: 20px;
+font-weight: 700;
+padding: 15px 25px;
+margin: 0px 15px;
+border-radius: 8px;
+color:#fff;
+background-color: rgb(29, 161, 101);
+}
+
+.pause{
+  font-size: 20px;
+font-weight: 700;
+padding: 15px 25px;
+margin: 0px 15px;
+border-radius: 8px;
+color:#fff;
+background-color: #CC2E5D;
+}
+
+.next, .prev {
+  font-size: 16px;
+font-weight: 700;
+padding: 10px 20px;
+margin: 0px 15px;
+border-radius: 6px;
+color:#fff;
+background-color: #FF5E5E;
+}
+.playlist{
+  padding: 0px 20px;
+
+}
+
+.playlist h3{
+  color:#212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 15px;
+  text-align: center;
+
+
+}
+
+
+.playlist .song{
+  display:block;
+  width: 100%;
+  padding: 15px;
+  font-size:20px;
+  font-weight: 700;
+  cursor:pointer;
+}
+
+.playlist .song:hover{
+  color:#ff5858;
+}
+.playlist .song.playing{
+  color:#fff;
+  background-image: linear-gradient(to right, #CC2E5D,#FF5858 );
 }
 </style>
